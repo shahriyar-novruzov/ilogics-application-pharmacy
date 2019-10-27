887e3a613fbb14bd607d40f157aea82d5db8ffb6
Shahriyar Novruzov <shahriyar.novruzov@pashabank.az>
2019-10-28 00:47:49 +0400

    JdbcTemplate

--- src/main/java/com/ilogics/application/pharmacy/repository/postgres/UserRepositoryPostgres.java
+++ src/main/java/com/ilogics/application/pharmacy/repository/postgres/UserRepositoryPostgres.java
@@ -5,46 +5,39 @@ import com.ilogics.application.pharmacy.repository.UserRepository;
 import com.ilogics.application.pharmacy.repository.mapper.UserRowMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserRepositoryPostgres implements UserRepository {
 
-    private NamedParameterJdbcTemplate template;
+    private JdbcTemplate template;
     private Logger logger = LoggerFactory.getLogger(UserRepositoryPostgres.class);
 
-    public UserRepositoryPostgres(NamedParameterJdbcTemplate template) {
+    public UserRepositoryPostgres(JdbcTemplate template) {
         this.template = template;
     }
 
     @Override
     public UserEntity findByUsername(String username) {
 
-        String sql = "select * from users where username = :username";
-        SqlParameterSource param = new MapSqlParameterSource()
-                .addValue("username", username);
-        List<UserEntity> entityList = template.query(sql, param, new UserRowMapper());
+        String sql = "select * from users where username = ?";
+        UserEntity entityList = template.queryForObject(sql, new Object[]{username}, new UserRowMapper());
 
         logger.debug("findByUsername user: {}", entityList);
 
-        return entityList != null && entityList.size() > 0 ? entityList.get(0) : null;
+        return entityList;
     }
 
     @Override
     public UserEntity findById(Long id) {
 
-        String sql = "select * from users where id = :id";
-        SqlParameterSource param = new MapSqlParameterSource()
-                .addValue("id", id);
-        List<UserEntity> entityList = template.query(sql, param, new UserRowMapper());
+        String sql = "select * from users where id = ?";
+
+        UserEntity entityList = template.queryForObject(sql, new Object[]{id}, new UserRowMapper());
 
         logger.debug("findById user: {}", entityList);
 
-        return entityList != null && entityList.size() > 0 ? entityList.get(0) : null;
+        return entityList;
     }
 }
